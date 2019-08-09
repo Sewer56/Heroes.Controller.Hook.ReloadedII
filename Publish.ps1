90d48160e882b4f4fd4222c589cbfe05ea44656f
Sewer56 <seweratwork@gmail.com>
2019-08-09 23:05:28 +0100

    Update publish script.

--- Publish.ps1
+++ Publish.ps1
@@ -1,27 +1,33 @@
 # Project Output Paths
-$hookOutputPath = "Heroes.Controller.Hook/bin"
-$XInputOutputPath = "Heroes.Controller.Hook.XInput/bin"
-$PostProcessOutputPath = "Heroes.Controller.Hook.PostProcess/bin"
+$hookOutputPath        = "Heroes.Controller.Hook/bin"
+$xInputOutputPath      = "Heroes.Controller.Hook.XInput/bin"
+$postProcessOutputPath = "Heroes.Controller.Hook.PostProcess/bin"
+
+$hookPublishName       	= "HeroesControllerHook.zip"
+$xinputPublishName      = "HeroesControllerXInput.zip"
+$postProcessPublishName = "HeroesControllerPostProcess.zip"
+
+$solutionName = "Heroes.Controller.Hook.sln"
 $publishDirectory = "Publish"
 
 if ([System.IO.Directory]::Exists($publishDirectory)) {
 	Get-ChildItem $publishDirectory -Include * -Recurse | Remove-Item -Force -Recurse
 }
 
-
 # Build
-dotnet clean Heroes.Controller.Hook.sln
-dotnet build -c Release Heroes.Controller.Hook.sln
+dotnet restore $solutionName
+dotnet clean $solutionName
+dotnet build -c Release $solutionName
 
 # Cleanup
 Get-ChildItem $hookOutputPath -Include *.pdb -Recurse | Remove-Item -Force -Recurse
 Get-ChildItem $hookOutputPath -Include *.xml -Recurse | Remove-Item -Force -Recurse
 
-Get-ChildItem $XInputOutputPath -Include *.pdb -Recurse | Remove-Item -Force -Recurse
-Get-ChildItem $XInputOutputPath -Include *.xml -Recurse | Remove-Item -Force -Recurse
+Get-ChildItem $xInputOutputPath -Include *.pdb -Recurse | Remove-Item -Force -Recurse
+Get-ChildItem $xInputOutputPath -Include *.xml -Recurse | Remove-Item -Force -Recurse
 
-Get-ChildItem $PostProcessOutputPath -Include *.pdb -Recurse | Remove-Item -Force -Recurse
-Get-ChildItem $PostProcessOutputPath -Include *.xml -Recurse | Remove-Item -Force -Recurse
+Get-ChildItem $postProcessOutputPath -Include *.pdb -Recurse | Remove-Item -Force -Recurse
+Get-ChildItem $postProcessOutputPath -Include *.xml -Recurse | Remove-Item -Force -Recurse
 
 # Make compressed directory
 if (![System.IO.Directory]::Exists($publishDirectory)) {
@@ -30,6 +36,6 @@ if (![System.IO.Directory]::Exists($publishDirectory)) {
 
 # Compress
 Add-Type -A System.IO.Compression.FileSystem
-[IO.Compression.ZipFile]::CreateFromDirectory( $hookOutputPath + '/Release', 'Publish/HeroesControllerHook.zip')
-[IO.Compression.ZipFile]::CreateFromDirectory( $XInputOutputPath + '/Release', 'Publish/HeroesControllerXInput.zip')
-[IO.Compression.ZipFile]::CreateFromDirectory( $PostProcessOutputPath + '/Release', 'Publish/HeroesControllerPostProcess.zip')
+[IO.Compression.ZipFile]::CreateFromDirectory( $hookOutputPath + '/Release', 'Publish/' + $hookPublishName)
+[IO.Compression.ZipFile]::CreateFromDirectory( $xInputOutputPath + '/Release', 'Publish/' + $xinputPublishName)
+[IO.Compression.ZipFile]::CreateFromDirectory( $postProcessOutputPath + '/Release', 'Publish/' + $postProcessPublishName)
